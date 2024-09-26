f5970497996231c2527f33625a362ec730c47a1f
amf673 <afedoruk@mtroyal.ca>
2024-09-26 12:34:51 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# Write your program in this file. Use the provided comments to help you organize your code
+
+#____CONSTANTS_________
+# constants (if any) should go in this section
+
+
+
+#____FUNCTIONS_________
+# functions (if used) should go in this section
+
+
+
+#____INPUTS____________
+# collect user inputs in this section
+
+
+
+
+#____PROCESSING_______
+# perform processing and calculations in this section
+
+
+
+
+#____OUTPUT___________
+# format and display your output in this section
