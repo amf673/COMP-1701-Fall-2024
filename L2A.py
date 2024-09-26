947e135b3b9f37427f2014e289375b0c1eb12fbc
amf673 <afedoruk@mtroyal.ca>
2024-09-26 12:08:13 -0600

    Create L2A.py

---
+++ L2A.py
@@ -0,0 +1,10 @@
+## Lab 2 COMP 1701 Fall 2024
+#
+# 
+
+first_name = input("Enter your first name: ")
+fav_animal = input("Enter your favourite animal: ")
+fav_year = input("Enter your favourite year: ")
+
+print(f"Name: \t\t\t", first_name,"\nFavourite Animal: \t", fav_animal, 
+      "\nFavourite Year: \t", fav_year)
