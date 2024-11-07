d93f8dac21ec506e612bc2c46ddc2bb112a77dbf
amf673 <afedoruk@mtroyal.ca>
2024-11-07 11:44:33 -0700

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+def fib_1 (n:int)->None:
+    """ print the first n fibonacci numbers """
+    if n == 0: 
+        print("0")
+    elif n == 1:
+        print("0, 1")
+    else:
+        print("0, 1", end="")
+        nminus1 = 1
+        nminus2 = 0
+        for i in range(2,n):
+            curr = nminus1 + nminus2
+            print( f", {curr}", end="")
+            nminus2 = nminus1 
+            nminus1 = curr
+        print()
+
+def main():
+  fib_1(50)
+
+main()
