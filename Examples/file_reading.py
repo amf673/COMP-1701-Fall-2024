ea1704b9cdd63438a16ed8d7899cfc1d52ae57d3
amf673 <afedoruk@mtroyal.ca>
2024-12-09 12:23:03 -0700

    Create file_reading.py

---
+++ Examples/file_reading.py
@@ -0,0 +1,32 @@
+# 
+# Ways to read a file 
+
+# These are equivalent:
+
+## 1. Line by line... 
+
+inpfile = open("nums.txt", "r")
+lines = []
+line = inpfile.readline()
+while line != "":
+   line = inpfile.readline()
+   lines.append(line)
+inpfile.close()
+print(lines)
+
+## 2. Using a for ... 
+inpfile = open("nums.txt", "r")
+lines = []
+for line in inpfile:
+    lines.append(line)
+inpfile.close()
+print(lines)
+
+## 3. Using readlines()... 
+inpfile = open("nums.txt", "r")
+lines = inpfile.readlines()
+inpfile.close()
+
+print(lines)
+
+inpfile.close()
